567a44bd90cc8287ea69660e8e46634369cc2962
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-17 22:45:05 -0500

    ciclos for y break

---
+++ java-primeros-pasos/src/buclesAnidados.java
@@ -0,0 +1,12 @@
+public class buclesAnidados {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                int result = i * j;
+                System.out.println("la si multiplicamos " + i + " x " + j + " = " + result);
+            }    
+        }
+
+    }
+}
---
+++ java-primeros-pasos/src/ciclosBreak.java
@@ -0,0 +1,14 @@
+public class ciclosBreak {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (j > i) {
+                    break;
+                }
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
